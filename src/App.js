07c1a0d83e8a0fca3e3f81ee36ceff44c10b0259
Co0lah <celes.thym@glaposte.net>
2022-10-02 03:53:41 +0200

    debounce implement

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import LazyLoad from "react-lazy-load";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import debounce from "lodash.debounce";
 
 const App = () => {
   const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [xloading, xsetLoading] = useState(false);
   const [items, setItems] = useState([]);
   const [monthlyItems, setMonthlyItems] = useState([]);
   const [search, setSearch] = useState("");
-  // const [isActive, setIsActive] = useState("hidden");
 
-  function getSanitizedSearchResults(list) {
-    return list.map((i) => i.show);
-  }
   const getSeriesList = async () => {
     try {
       const response = await fetch(`https://api.tvmaze.com/shows`);
@@ -25,8 +23,7 @@ const App = () => {
       }
       const result = await response.json();
       const res = await popresponse.json();
-      // console.log(res);
-      setIsLoaded(true);
+      setLoading(true);
       setItems(result);
       setMonthlyItems(res);
       setError(null);
@@ -36,43 +33,35 @@ const App = () => {
     }
   };
 
-  const getSearchResults = async () => {
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${search}`
-    );
-    const result = await response.json();
-    setItems(getSanitizedSearchResults(result));
-  };
-
-  // const getPopularSearch = async () => {
-  //   const response = await fetch(`https://api.tvmaze.com/shows?page=10`);
-  //   const res = await response.json();
-  //   setMonthlyItems(res);
-  // };
-
   useEffect(() => {
     if (!search) {
       getSeriesList();
     }
-    search.length && getSearchResults();
-    // if (window.scrollY > 200) {
-    //   setIsActive("visible");
-    //   console.log(window.scroll);
-    // }
+    async function getSearchResults() {
+      // A CHECK
+      const response = await fetch(
+        `https://api.tvmaze.com/search/shows?q=${search}`
+      );
+      xsetLoading(true);
+      const result = await response.json();
+      const res = result.map((i) => i.show);
+      setItems(res);
+      xsetLoading(false);
+    }
+    if (search) getSearchResults();
   }, [search]);
 
-  // useEffect(() => {
-  //   getPopularSearch();
-  // }, []);
+  const debounced = (e) => setSearch(e?.target?.value);
+
+  const debouncedSearch = debounce(debounced, 500);
 
   const getRandomKey = () => {
     return Math.random(999999);
   };
 
-  if (!isLoaded) {
+  if (!loading) {
     return (
       <div className="spinner-container">
-        <h2>Loading</h2>
         <div className="loading-spinner"></div>
       </div>
     );
@@ -80,117 +69,115 @@ const App = () => {
 
   return (
     <div className="wrapper">
-      <LazyLoad>
-        <div className="wrapper-container">
-          <header className="">
-            <h1>
-              <a href="#">SERIES SEEKERS</a>
-            </h1>
-            <input
-              type="search"
-              name="search-form"
-              id="search-form"
-              className="search-input"
-              placeholder="Search for..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            {error && (
-              <div>{`There is a problem fetching the requested serie  - ${error}`}</div>
-            )}
-          </header>
-          <div className="main-series-container">
-            <div className="main-series">
-              <h3>Series streaming</h3>
-              <ul>
-                {items.map(({ name, image, summary, premiered, rating }) => (
-                  <li key={getRandomKey()}>
-                    <div className="overlay-image">
-                      <a href="#">
-                        <div className="image-container">
-                          <img
-                            className="image"
-                            src={
-                              image?.medium ||
-                              image?.original ||
-                              "https://images.pexels.com/photos/2335046/pexels-photo-2335046.jpeg"
-                            }
-                            alt="Alt text"
-                          />
-                          <div className="premiered">
-                            <p>{premiered?.substring(0, 4)}</p>
-                          </div>
-                        </div>
-                        <div className="hover">
-                          <div className="text">
-                            <b>Synopsis: </b>
-                            <p
-                              dangerouslySetInnerHTML={{
-                                __html: summary?.substring(0, 150),
-                              }}
-                            />
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div className="infos">
-                      <p>{name?.substring(0, 20)}</p>
-                      <p>
-                        <span>rating </span>
-                        <b>{rating?.average || "N/A"}</b>
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="aside-series">
-              <h3>Popular Search</h3>
-              <ul className="various-container">
-                {monthlyItems.map(({ name, image, premiered }) => (
-                  <li key={getRandomKey()}>
-                    <div className="overlay-image">
-                      <a href="#">
-                        <div className="image-container">
-                          <img
-                            className="image"
-                            src={
-                              image?.medium ||
-                              image?.original ||
-                              "https://images.pexels.com/photos/2335046/pexels-photo-2335046.jpeg"
-                            }
-                            alt="Alt text"
-                          />
-                          <div className="premiered">
-                            <p>{premiered?.substring(0, 4)}</p>
-                          </div>
-                        </div>
-                        <div className="hover">
-                          {/* <div className="text">
-                            <b>Synopsis: </b>
-                            <p
-                              dangerouslySetInnerHTML={{
-                                __html: summary?.substring(0, 150),
-                              }}
-                            />
-                          </div> */}
+      <div className="wrapper-container">
+        <header className="">
+          <h1>
+            <a href="#">SERIES SEEKERS</a>
+          </h1>
 
-                          <div className="svg">
-                            <FontAwesomeIcon icon={faCirclePlay} />
+          <input
+            autoFocus
+            type="search"
+            name="search-form"
+            id="search-form"
+            className="search-input"
+            placeholder="Search for..."
+            onChange={debouncedSearch}
+          />
+          {error && (
+            <div>{`There is a problem fetching the requested serie  - ${error}`}</div>
+          )}
+        </header>
+        <div className="main-series-container">
+          <div className="main-series">
+            {xloading && (
+              <div className="spinner-container-search">
+                <div className="loading-spinner"></div>
+              </div>
+            )}
+            <h3>Series streaming</h3>
+            <ul>
+              {items.map(({ name, image, summary, premiered, rating }) => (
+                <li key={getRandomKey()}>
+                  <div className="overlay-image">
+                    <a href="#">
+                      <div className="image-container">
+                        <LazyLoad>
+                          <img
+                            className="image"
+                            src={
+                              image?.medium ||
+                              image?.original ||
+                              "https://images.pexels.com/photos/2335046/pexels-photo-2335046.jpeg"
+                            }
+                            alt="Alt text"
+                          />
+                        </LazyLoad>
+                      </div>
+                      <div className="hover">
+                        <div className="text">
+                          <p style={{ color: "#00b7ff" }}>
+                            {name?.substring(0, 20).toUpperCase()}
+                          </p>
+                          <p
+                            dangerouslySetInnerHTML={{
+                              __html: summary?.substring(0, 150),
+                            }}
+                          />
+                        </div>
+                        <div className="series-infos">
+                          <div className="sub-infos">
+                            <p>{premiered?.substring(0, 4)}</p>
+                            <p>
+                              <span>rating </span>
+                              <b>{rating?.average || "N/A"}</b>
+                            </p>
                           </div>
                         </div>
-                      </a>
-                    </div>
-                    <div>
-                      <p>{name?.substring(0, 20)}</p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+                      </div>
+                    </a>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="aside-series">
+            <h3>Popular Search</h3>
+            <ul className="various-container">
+              {monthlyItems.map(({ name, image, premiered }) => (
+                <li key={getRandomKey()}>
+                  <div className="overlay-image">
+                    <a href="#">
+                      <div className="image-container">
+                        <img
+                          className="image"
+                          src={
+                            image?.medium ||
+                            image?.original ||
+                            "https://images.pexels.com/photos/2335046/pexels-photo-2335046.jpeg"
+                          }
+                          alt="Alt text"
+                        />
+                        <div className="premiered">
+                          <p>{premiered?.substring(0, 4)}</p>
+                        </div>
+                      </div>
+                      <div className="hover">
+                        <div className="svg">
+                          <FontAwesomeIcon icon={faCirclePlay} />
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div>
+                    <p>{name?.substring(0, 20)}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </LazyLoad>
+      </div>
       {/* <div id="scrollTop" className={isActive}></div> */}
     </div>
   );
